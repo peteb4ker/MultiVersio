0add8fdb8f1c9f2921da9858b6de9e550914d767
peteb4ker <peteb4ker@users.noreply.github.com>
2023-12-30 21:28:12 +0000

    deploy: f1b721962e760f47e6b57a11463c8cdd9b8d2538

--- classMultiVersio.js
+++ classMultiVersio.js
@@ -1,5 +1,6 @@
 var classMultiVersio =
 [
     [ "getMode", "classMultiVersio.html#ab24f10a18995ffdf733f5eaf18f8335b", null ],
+    [ "processAudio", "classMultiVersio.html#aa0fb4d816f87db93492346f952b013c3", null ],
     [ "run", "classMultiVersio.html#a52a36d29178b46e0e385f48f74e507fb", null ]
 ];
